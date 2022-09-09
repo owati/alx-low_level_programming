7bfd18a85083f5e8a76b5bfbe9e82345243d8e45
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 15:14:02 -0700

    ready

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)", sizeof(int));
 	printf("Size of a long int: %d byte(s)", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
-	printf("Size of a float: %d byte(s)", sizeof(float);
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 }
