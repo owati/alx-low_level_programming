1e7aa4cba10eb28f3061a189b8b4064823256205
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 14:52:44 -0700

    ready

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to the screen
+ *
+ * Return: zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
