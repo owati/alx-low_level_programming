8b851bfb903f6275baf643f1f322eef36165c149
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 15:53:48 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints to std error
+ *
+ *   * 
+ *
+ *    * Return: always 0
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+	    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	        return (0);
+
+}
