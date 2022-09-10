9a73ee3f0d2a12134c49e5f535812ab4219545db
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 18:03:17 -0700

    ready

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 49; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
