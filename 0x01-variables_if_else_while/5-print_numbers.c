d5c9244d180c09f14059b0aa9ee6a009b9603f77
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 17:57:25 -0700

    ready

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 9; num++)
+	{
+		printf("%i",num);
+	}
+	printf('\n');
+	return (0);
+}
