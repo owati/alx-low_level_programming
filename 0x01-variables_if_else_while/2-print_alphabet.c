74b6d4d11437945c63f097bbeab017c3c9d7cad3
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 17:41:03 -0700

    Ready

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	for (int num = 97; num < 123; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
