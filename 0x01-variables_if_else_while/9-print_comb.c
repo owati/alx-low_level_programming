90db152e3f579bd36fef2c2dfe0f739bc4857475
owati <mofiyinfoluwaowati@gmail.com>
2022-09-09 18:19:03 -0700

    ready

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the alphabets
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
