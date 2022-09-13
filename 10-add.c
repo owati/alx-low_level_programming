c614ff476868bed772eca2ed676a3ef828e030dd
owati <mofiyinfoluwaowati@gmail.com>
2022-09-13 16:13:40 -0700

    ready

---
+++ 10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two number together
+ * @a: the first number
+ * @b: the seconf number
+ *
+ * Return: the added number
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
