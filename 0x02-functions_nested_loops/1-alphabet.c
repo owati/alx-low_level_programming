aca3516be6090403f59c1c6e48fb04308e49c35b
owati <mofiyinfoluwaowati@gmail.com>
2022-09-13 06:22:49 -0700

    ready

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the alphabets
+ *
+ * Return: void
+*/
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i)
+	}
+	_putchar('\n');
+	return (0);
+}
