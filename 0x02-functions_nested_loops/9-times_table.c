64f438633b4259585f7a32e61873d6b65c66a223
owati <mofiyinfoluwaowati@gmail.com>
2022-09-13 15:59:49 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints out the times table
+ *
+ * Return: void
+*/
+
+void times_table(void)
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			int mult = row * col;
+
+			if (col == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+				continue;
+			}
+			if (mult > 9)
+			{
+				_putchar('0' + (int)(mult / 10));
+				_putchar('0' + (int)(mult % 10));
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + mult);
+			}
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
