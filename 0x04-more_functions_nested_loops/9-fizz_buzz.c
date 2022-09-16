7b8948a77bb6fc2cc1d4051d93caf96f6baf8529
owati <mofiyinfoluwaowati@gmail.com>
2022-09-15 21:51:29 -0700

    ready

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - the fizzbuzz test
+ * Return: 0
+*/
+
+int main(void)
+{
+	int start;
+
+	for (start = 1; start <= 100; start++)
+	{
+		if (start % 15 == 0)
+			printf("FizzBuzz ");
+		else if (start % 5 == 0)
+			printf("Buzz ");
+		else if (start % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", start);
+	}
+	printf("\n");
+}
