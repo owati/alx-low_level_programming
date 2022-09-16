a31e778729fa21b73624df43ddfbc8a17898ea76
owati <mofiyinfoluwaowati@gmail.com>
2022-09-15 21:37:21 -0700

    ready

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of size n
+ * @size: the size of the square
+ * Return: void
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar('*');
+		_putchar('\n');
+	}
+}
