d008b3a4e835d133290b87c69ec30d594ff36f09
owati <mofiyinfoluwaowati@gmail.com>
2022-09-15 21:08:38 -0700

    I am ready

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int count, i;
 
-	for(count = 0; count < 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i / 10)
 				_putchar('1');
-			_putchar(i % 10);
+			_putchar('0' + i % 10);
 		}
 		_putchar('\n');
 	}
