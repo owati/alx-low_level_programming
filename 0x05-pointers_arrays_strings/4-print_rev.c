251aa10301ccddcb5499b5495971ce64b1eeb09c
owati <mofiyinfoluwaowati@gmail.com>
2022-09-19 20:49:26 -0700

    I am ready

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be printed
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+	char *start, *end, temp;
+	int i;
+
+	start = s;
+	end = s;
+	for (i = 0; i < l - 1; i++)
+		end++;
+
+	for (i = 0; i < l / 2; i++)
+	{
+		temp = *start;
+		*start = *end;
+		*end = temp;
+		start++;
+		end--;
+	}
+
+	while (*s)
+		_putchar(*s);
+	_putchar('\n');
+}
