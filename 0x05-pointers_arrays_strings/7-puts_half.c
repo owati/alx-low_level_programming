8c917dd68aed56278c3bb05d887a67de79fcad37
owati <mofiyinfoluwaowati@gmail.com>
2022-09-19 22:04:19 -0700

    ready

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of the string
+ * @str: steh string
+ * Return: void
+*/
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int start;
+
+	if (len % 2)
+		start = len / 2;
+	else
+		start = (len - 1) / 2;
+	while (*(str + start))
+	{
+		_putchar(*(str + start));
+		start++;
+	}
+	_putchar('\n');
+}
