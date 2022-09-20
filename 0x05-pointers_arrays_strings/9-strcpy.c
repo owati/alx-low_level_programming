81b1654f55005bc05ab9c6990381e649f4e57e5e
owati <mofiyinfoluwaowati@gmail.com>
2022-09-20 16:28:44 -0700

    ready

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy on string to another
+ * @dest: the destination string
+ * @src: the source string
+ * Return: char pointer
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+}
