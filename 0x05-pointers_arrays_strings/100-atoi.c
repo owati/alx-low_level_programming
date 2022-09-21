ba22109bf0149e4e0d0acfa285dc46b6fa589aac
owati <mofiyinfoluwaowati@gmail.com>
2022-09-20 17:24:08 -0700

    ready

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: the number
+*/
+
+int _atoi(char *s)
+{
+	int len = strlen(s);
+	bool positive = true;
+	bool hasSeenNumber = false;
+	int sum = 0;
+	int count = 0;
+
+	while (count < len)
+	{
+		int num_equiv;
+		char letter = *(s + count);
+
+		if (letter < '0' && letter > '9')
+		{
+			if (hasSeenNumber)
+				break;
+			if (letter == '-')
+				positive = !positive;
+			continue;
+		}
+		hasSeenNumber ? hasSeenNumber : true;
+		num_equiv = letter - '0';
+		sum = sum * pow(10, count) + num_equiv;
+		count++;
+	}
+	return (positive ? sum : -1 * sum);
+}
