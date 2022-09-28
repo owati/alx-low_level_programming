0fa700ce5baa2e65d96c1604b5c0ed702f0296e5
owati <mofiyinfoluwaowati@gmail.com>
2022-09-27 20:36:57 -0700

    ready

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * _strspn - find s apsn ina string
+ * @s: the string
+ * @accept: the check string
+ * Return: the position number
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int num = 0;
+
+	for (; s[num]; num++)
+	{
+		unsigned int loop =  0;
+		bool check = false;
+
+		for (; accept[loop]; loop++)
+		{
+			if (accept[loop] == s[num])
+			{
+				check = true;
+				break;
+			}
+		}
+		if (!check)
+			return (num + 1);
+	}
+	return (0);
+}
