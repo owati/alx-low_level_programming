873ebbbef112e8b109158f9c1fee9ccb945cd424
owati <mofiyinfoluwaowati@gmail.com>
2022-09-28 09:20:42 -0700

    ready

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - finds the factorial a positive integer
+ * @n: the number
+ * Return: the factorial
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (0);
+	else
+		return (n * factorial(n - 1));
+}
