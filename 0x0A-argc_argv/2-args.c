f699492373ff6539613935e37fe6bf1841c3d3bd
owati <mofiyinfoluwaowati@gmail.com>
2022-09-30 14:06:35 -0700

    ready

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name off the file
+ * @argc: the lenght of the commandlinr
+ * @argv: the command line arguements
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int  i;
+	
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[0]);
+	return (0);
+}
