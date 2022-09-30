17c0dc01f19b9c7bc24152a521cf5204df2fd149
owati <mofiyinfoluwaowati@gmail.com>
2022-09-30 13:43:26 -0700

    I am ready

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the name off the file
+ * main - prints the number of argurments
  * @argc: the lenght of the commandlinr
  * @argv: the command line arguements
  * Return: 0
 */
 
-int main(int argc, char *argv[][)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc);
 	return (0);
 }
