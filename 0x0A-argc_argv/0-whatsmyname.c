1a45a2b0c3c418db6a27e1d304a5722cb659498f
owati <mofiyinfoluwaowati@gmail.com>
2022-09-30 01:11:12 -0700

    ready

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name off the file
+ * @argc: the lenght of the commandlinr
+ * @argv: the command line arguements
+ * Return: 0
+*/
+
+int main(int argv, char *argc[])
+{
+	printf(argc[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Argc and argc
