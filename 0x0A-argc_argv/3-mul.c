c00fd0440fba233b404706118146ddb8b6f63bd1
owati <mofiyinfoluwaowati@gmail.com>
2022-10-01 12:34:40 -0700

    ready

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that calculates the multiplication of two numbers
+ * @argc: the number of arguement provided
+ * @argv: the array of the arguements
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int mult = 1;
+
+	for (; i < argc; i++)
+		mult *= atoi(argv[i]);
+	printf("%d\n", mult);
+	return (0);
+}
