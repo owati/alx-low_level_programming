ce4767651e4fdf582d95a859d57212aa73481e43
owati <mofiyinfoluwaowati@gmail.com>
2022-10-05 21:48:22 -0700

    ready

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	len = strlen(str);
+	len = strlen(str) + 1;
 	ptr = malloc(len * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
