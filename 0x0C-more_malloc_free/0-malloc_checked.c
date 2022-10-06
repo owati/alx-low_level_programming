23c62004002f12bffc83a9ed22ff59ec9f572ff0
owati <mofiyinfoluwaowati@gmail.com>
2022-10-06 09:59:35 -0700

    ready

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - creates apointer with malloc
+ * @b: the size of the memory
+ * Return: a void poiter
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
