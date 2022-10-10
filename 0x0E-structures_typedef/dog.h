aa2bf79562ea6020cce75eb64ae563f87a3b24e5
owati <mofiyinfoluwaowati@gmail.com>
2022-10-10 09:41:35 -0700

    ready

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog structure
+ * @name: the name
+ * @age: the age
+ * @owner: the owner
+*/
 struct dog
 {
-	char *dog;
+	char *name;
 	float age;
 	char *owner;
 };
+#endif
