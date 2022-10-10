3d418f3f9cbbba01afd9ee8e377198e52e1f4b68
owati <mofiyinfoluwaowati@gmail.com>
2022-10-10 10:04:21 -0700

    ready

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,11 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
-		if (d->age)
+		printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+		if (d->age != NULL)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: (nil)");
-		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+		printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 	}
 }
