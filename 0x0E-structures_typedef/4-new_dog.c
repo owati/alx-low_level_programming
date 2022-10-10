60c0a650c59ba73027711ed57f3cfa711af37b56
owati <mofiyinfoluwaowati@gmail.com>
2022-10-10 16:10:37 -0700

    Ready

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!name || age < 0 || !owner)
 		return (NULL);
-	dog = malloc(sizeof(dog));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 	dog->name = malloc(strlen(name) + 1);
