be137f9e5b23c373cfa2bdafe14e12bb5e5a4d99
owati <mofiyinfoluwaowati@gmail.com>
2022-10-12 14:04:00 -0700

    ready

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,5 +2,6 @@
 #define FUNCTION
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+typedef size_t int;
 void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
