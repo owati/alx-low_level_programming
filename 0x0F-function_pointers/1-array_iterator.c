210f7a06d7b026864f180c6bc9ecf76f793d847c
owati <mofiyinfoluwaowati@gmail.com>
2022-10-12 14:39:24 -0700

    ready

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	if (action != NULL && array != NULL && size >= 0)
+	if (action != NULL && array != NULL)
 	{
 		while (i < size)
 		{
