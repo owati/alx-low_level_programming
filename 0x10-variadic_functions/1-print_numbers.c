ed10d89e4dad48d86359bca91a74216496a6ccff
owati <mofiyinfoluwaowati@gmail.com>
2022-10-13 17:40:18 -0700

    ready

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(ptr, int));
 		if (separator != NULL && i != (n - 1))
-			printf(separator);
+			printf("%s", separator);
 	}
 	va_end(ptr);
 	printf("\n");
